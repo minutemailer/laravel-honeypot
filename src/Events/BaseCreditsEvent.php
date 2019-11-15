55e5094f16b4e8afa4591ff3cf40172ce7b056e9
Carlos Egusquiza <carlosea@kth.se>
2019-11-15 11:32:08 +0100

    Add events CreditsAdded and CreditsUsed

---
+++ src/Events/BaseCreditsEvent.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Minutemailer\Shared\Events;
+
+use Illuminate\Queue\SerializesModels;
+use Minutemailer\Honeypot\Models\CreditBucket;
+
+class BaseCreditsEvent
+{
+    use SerializesModels;
+
+    protected $bucket;
+    protected $amount;
+    protected $message;
+
+    public function __construct(CreditBucket $bucket, int $amount)
+    {
+        $this->bucket = $bucket;
+        $this->amount = $amount;
+    }
+
+    public function getBucket(): CreditBucket
+    {
+        return $this->bucket;
+    }
+
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+}
---
+++ src/Events/CreditsAdded.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Minutemailer\Shared\Events;
+
+class CreditsAdded extends BaseCreditsEvent
+{
+    //
+}
